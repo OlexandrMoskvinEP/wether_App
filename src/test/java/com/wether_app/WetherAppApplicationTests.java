00dfa1656d4fac6c0bfd89d3c651d22fd7c8adbe
Olexandr Moskvin <olexandr_moskvin@epam.com>
2025-08-25 09:18:00 +0300

    initial commit

---
+++ src/test/java/com/wether_app/WetherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wether_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
